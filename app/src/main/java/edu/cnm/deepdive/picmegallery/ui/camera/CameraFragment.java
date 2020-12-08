2789a52ffdda098c140ff44c160e349c8711bf59
isaac <shifdub@gmail.com>
2020-12-08 08:41:33 -0700

    updated camera fragment

--- app/src/main/java/edu/cnm/deepdive/picmegallery/ui/camera/CameraFragment.java
+++ app/src/main/java/edu/cnm/deepdive/picmegallery/ui/camera/CameraFragment.java
@@ -1,34 +1,63 @@
 package edu.cnm.deepdive.picmegallery.ui.camera;
 
+import static android.app.Activity.RESULT_CANCELED;
+import static android.app.Activity.RESULT_OK;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import edu.cnm.deepdive.picmegallery.R;
+import edu.cnm.deepdive.picmegallery.databinding.FragmentCameraBinding;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class CameraFragment extends Fragment {
+  public static final String EXTRA_INFO = "default";
+  private Button btnCapture;
+  private ImageView imgCapture;
+  private static final int Image_Capture_Code = 1;
 
-  private CameraViewModel cameraViewModel;
+  @Nullable
+  @Override
+  public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    View view = inflater.inflate(R.layout.fragment_camera, container, false);
+    btnCapture =(Button) view.findViewById(R.id.captureButton);
 
-  public View onCreateView(@NonNull LayoutInflater inflater,
-      ViewGroup container, Bundle savedInstanceState) {
-    cameraViewModel =
-        new ViewModelProvider(this).get(CameraViewModel.class);
-    View root = inflater.inflate(R.layout.fragment_camera, container, false);
-    final TextView textView = root.findViewById(R.id.nav_camera);
-    cameraViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+    btnCapture.setOnClickListener(new View.OnClickListener() {
       @Override
-      public void onChanged(@Nullable String s) {
-        textView.setText(s);
+      public void onClick(View v) {
+        Intent cInt = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cInt,Image_Capture_Code);
       }
     });
-    return root;
+
+    return view;
   }
-}
+
+  @Override
+  public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    if (requestCode == Image_Capture_Code) {
+      if (resultCode == RESULT_OK) {
+        Bitmap bp = (Bitmap) data.getExtras().get("data");
+        imgCapture.setImageBitmap(bp);
+      } else if (resultCode == RESULT_CANCELED) {
+        Toast.makeText(getActivity(), "Cancelled", Toast.LENGTH_LONG).show();
+      }
+    }
+  }}
